266f975a005d4de0b889692dcbe94849e11abacc
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-07-14 12:34:20 +0930

    Initial implementation.

--- scraper.js
+++ scraper.js
@@ -1,59 +1,101 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+// Parses the development application at the South Australian City of Mitcham web site and places them
+// in a database.
+//
+// Michael Bone
+// 14th July 2018
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+let cheerio = require("cheerio");
+let request = require("request");
+let sqlite3 = require("sqlite3").verbose();
+let urlparser = require("url");
+let moment = require("moment");
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+const DevelopmentApplicationsUrl = "https://eproperty.mitchamcouncil.sa.gov.au/T1PRProd/WebApps/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L28&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL28";
+const CommentUrl = "mailto:mitcham@mitchamcouncil.sa.gov.au";
+
+// Sets up an sqlite database.
+
+function initializeDatabase(callback) {
+    let database = new sqlite3.Database("data.sqlite");
+    database.serialize(() => {
+        database.run("create table if not exists [data] ([council_reference] text primary key, [address] text, [description] text, [info_url] text, [comment_url] text, [date_scraped] text, [date_received] text, [on_notice_from] text, [on_notice_to] text)");
+        callback(database);
+    });
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+// Inserts a row in the database if it does not already exist.
+
+function insertRow(database, developmentApplication) {
+    let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    sqlStatement.run([
+        developmentApplication.applicationNumber,
+        developmentApplication.address,
+        developmentApplication.reason,
+        developmentApplication.informationUrl,
+        developmentApplication.commentUrl,
+        developmentApplication.scrapeDate,
+        null,
+        null,
+        developmentApplication.onNoticeToDate
+    ], function(error, row) {
+        if (error)
+            console.log(error);
+        else {
+            if (this.changes > 0)
+                console.log(`    Inserted new application \"${developmentApplication.applicationNumber}\" into the database.`);
+            sqlStatement.finalize();  // releases any locks
+        }
+    });
 }
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+// Reads a page using a request.
+    
+function requestPage(url, callback) {
+    console.log(`Requesting page: ${url}`);
+    request(url, (error, response, body) => {
+        if (error)
+            console.log(`Error requesting page ${url}: ${error}`);
+        else
+            callback(body);
+    });
 }
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
+// Parses the page at the specified URL.
 
-		callback(body);
-	});
+function run(database) {
+    let url = DevelopmentApplicationsUrl;
+    let parsedUrl = new urlparser.URL(url);
+    let baseUrl = parsedUrl.origin + parsedUrl.pathname;
+
+    
+    requestPage(url, body => {
+        // Use cheerio to find all development applications listed in the page.
+ 
+        let $ = cheerio.load(body);
+        $("div.list-container a").each((index, element) => {
+            // Each development application is listed with a link to another page which has the
+            // full development application details.
+
+            let developmentApplicationUrl = new urlparser.URL(element.attribs.href, baseUrl).href;
+            requestPage(developmentApplicationUrl, body => {
+                // Extract the details of the development application from the development
+                // application page and then insert those details into the database as a row
+                // in a table.  Note that the selectors used below are based on those from the
+                // https://github.com/LoveMyData/burnside scraper.
+
+                let $ = cheerio.load(body);
+
+                insertRow(database, {
+                    applicationNumber: $("span.field-label:contains('Application number') ~ span.field-value").text().trim(),
+                    address: $("span.field-label:contains('Address') ~ span.field-value").text().replace("View Map", "").trim(),
+                    reason: $("span.field-label:contains('Nature of development') ~ span.field-value").text().trim(),
+                    informationUrl: developmentApplicationUrl,
+                    commentUrl: CommentUrl,
+                    scrapeDate: moment().format("YYYY-MM-DD"),
+                    onNoticeToDate: moment($("h2.side-box-title:contains('Closing Date') + div").text().split(',')[0].trim(), "D MMMM YYYY", true).format("YYYY-MM-DD") });
+            });
+        });
+    });
 }
 
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+initializeDatabase(run);
